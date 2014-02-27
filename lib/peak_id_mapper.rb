1b2531b9151952eae4cefb786cb02aeef2069571
John Prince <jtprince@gmail.com>
2014-02-27 16:01:22 -0700

    fixed bug with ms_level including too much and made more efficient

--- lib/peak_id_mapper.rb
+++ lib/peak_id_mapper.rb
@@ -14,7 +14,6 @@ GLOBALJOIN = ";"
 PpmThreshold = 100
 RTThreshold = 400 # amount forward and backward to search
 IntensityThreshold = 500
-Matches = Struct.new(:retention_times, :mzs, :intensities)
 # determine_stats returns [min, max, mean, sample_variance, standard_deviation, sum, size]
 PeakIDMap = Struct.new(:aaseq, :proteins, :mods, :charge, :mh, :ion_score, :ppm_error, :spectrum_file, :match_file, :rt, :mz, :int, :rt_array, :mz_array, :int_array) # Data = [rt_array, mz_array, int_array]
 HEADERLINE = %w{Sequence Proteins RTmin RTmax RTmean RTvariance RTstdev RTsum RTsize Modifications Charge MZmin MZmax MZmean MZvariance MZstdev MZsum MZsize MH+ deltaM(ppm) Ion_Score SpectrumFile MatchFile Imin Imax Imean Ivariance Istdev Isum Isize RT MZ Intensities RT_array MZ_array Int_array}.join(GLOBALJOIN)
@@ -64,88 +63,85 @@ class PeakIDMapper
     putsv "Failure" unless File.exists?(file)
   end
   class PepxmlParser
-    # This is from the run_compare code I wrote
+    # returns an array of pep identifications (PepID's).
     def self.parse(file)
       putsv "Parsing the pepxml #{file}"
       doc = Nokogiri.XML(File.open(file))
-      pepids = []
       search_sum = doc.xpath("//xmlns:search_summary") # search for aminoacid_modification and terminal_modification
-      doc.xpath('//xmlns:search_hit[@hit_rank=$value]', nil, {value: '1'}).each do |search_hit|
+      doc.xpath('//xmlns:search_hit[@hit_rank=$value]', nil, {value: '1'}).map do |search_hit|
         spec_query = search_hit.parent.parent
-        output = PepID.new
-        output.scan_num = spec_query.attributes["start_scan"].value.to_i 
-        output.charge = spec_query.attributes["assumed_charge"].value.to_i
+        pep_id = PepID.new
+        pep_id.scan_num = spec_query.attributes["start_scan"].value.to_i 
+        pep_id.charge = spec_query.attributes["assumed_charge"].value.to_i
         mod_info = search_hit.css "modification_info"
         unless mod_info.empty?
-          output.mods = mod_info.map do |mod| 
+          pep_id.mods = mod_info.map do |mod| 
             tmp = mod.attributes.entries
             tmp.map {|f,l| [f, mod.attributes[f].value.to_f]}
           end
         end
-        output.precursor_neutral_mass = spec_query.attributes["precursor_neutral_mass"].value.to_f
-        output.retention_time = spec_query.attributes["start_scan"].value.to_i
-        output.proteins = search_hit.attributes["protein"].value
-        output.aaseq = search_hit.attributes["peptide"].value
-        output.match_file = file
-        output.ion_score = search_hit.css("search_score").to_s[/"ionscore" value="(.*?)"\/>/,1].to_f
-        output.mh = search_hit.attributes["calc_neutral_pep_mass"].value.to_f
-        output.ppm_error = search_hit.attributes["massdiff"].value.to_f
-        pepids << output
+        pep_id.precursor_neutral_mass = spec_query.attributes["precursor_neutral_mass"].value.to_f
+        pep_id.retention_time = spec_query.attributes["start_scan"].value.to_i
+        pep_id.proteins = search_hit.attributes["protein"].value
+        pep_id.aaseq = search_hit.attributes["peptide"].value
+        pep_id.match_file = file
+        pep_id.ion_score = search_hit.css("search_score").to_s[/"ionscore" value="(.*?)"\/>/,1].to_f
+        pep_id.mh = search_hit.attributes["calc_neutral_pep_mass"].value.to_f
+        pep_id.ppm_error = search_hit.attributes["massdiff"].value.to_f
+        pep_id
       end
-      pepids#.uniq {|a| a.aaseq} ## PROBABLY a bad idea
     end
   end
 
   class MzmlParser
-    SpectralObject = Struct.new(:peaks, :retention_time)
-    def self.join_pepxml_with_mzml_file(pepdata, file)
+    # spectrum_array is [mzs, intensities]
+    SpectralObject = Struct.new(:spectrum_array, :retention_time)
+    def self.join_pepxml_with_mzml_file(pep_ids, file)
       putsv "Joining the pepids with the mzml"
       output_map = []
       # This is the file which you must cache.  
       # Basically, pre-read the file into memory and then play with the structures you've cached inside the loop
       # The problem otherwise is that the random access abilities of Mspire reek havoc on the CPU cycles.
-      
+
       # I would do something like this:
       spectra = []
-      Mspire::Mzml.open(file) do |mzml|
-        mzml.each_spectrum do |spectrum|
-          spectra << SpectralObject.new(spectrum.peaks, spectrum.retention_time)
+      Mspire::Mzml.foreach(file) do |spectrum|
+        if spectrum.ms_level == 1
+          spectra << SpectralObject.new(spectrum.mzs_and_intensities, spectrum.retention_time)
         end
       end
       putsv "Finished reading the file, and have now closed the MZML"
       # Now,do the following loops using the previous data instead...
-      
-      pepdata.each do |pepid|
+
+      pep_ids.each do |pep_id|
         rt_array, mz_array, int_array = [],[],[]
-        mass = pepid.precursor_neutral_mass
+        mass = pep_id.precursor_neutral_mass
         mass_range = ppm_range(mass, PpmThreshold)
-        time_range = (pepid.retention_time-RTThreshold)..(pepid.retention_time+RTThreshold)
-        spectra.each do |spectralobject| # |peaks, retention_time|
+        time_range = (pep_id.retention_time-RTThreshold)..(pep_id.retention_time+RTThreshold)
+        spectra.each do |spectralobject|
           next if spectralobject.peaks.empty?
-          # ... continued.
-            next unless time_range.include?(spectralobject.retention_time)
-            list = spectralobject.peaks
-            rrange = list.transpose.first.bsearch_range do |a| 
-              mass_range.include?(a) ? 0 : a <=> mass
-            end
-            resp = spectralobject.peaks[rrange].flatten #select {|a| puts "mz: #{a.first}"; puts "ppm: #{ppm(a.first,mass)}"; ppm(a.first,mass) < PpmThreshold}
-            next unless mass_range.include?(resp.first)
-            next if resp.first == nil
-            next if resp.last < IntensityThreshold
-            rt_array << spectralobject.retention_time
-            mz_array << resp.first
-            int_array << resp.last
+          next unless time_range.include?(spectralobject.retention_time)
+          rrange = spectralobject.first.bsearch_range do |a| 
+            mass_range.include?(a) ? 0 : a <=> mass
           end
-          next if mz_array.empty?
-          output_map << PeakIDMap.new(pepid.aaseq, pepid.proteins, pepid.mods, pepid.charge, pepid.mh, pepid.ion_score, pepid.ppm_error, file, pepid.match_file, PeakIDMapper.determine_stats(rt_array), PeakIDMapper.determine_stats(mz_array), PeakIDMapper.determine_stats(int_array), rt_array, mz_array, int_array)
+          resp = spectralobject.peaks[rrange].flatten #select {|a| puts "mz: #{a.first}"; puts "ppm: #{ppm(a.first,mass)}"; ppm(a.first,mass) < PpmThreshold}
+          next unless mass_range.include?(resp.first)
+          next if resp.first == nil
+          next if resp.last < IntensityThreshold
+          rt_array << spectralobject.retention_time
+          mz_array << resp.first
+          int_array << resp.last
         end
+        next if mz_array.empty?
+        output_map << PeakIDMap.new(pep_id.aaseq, pep_id.proteins, pep_id.mods, pep_id.charge, pep_id.mh, pep_id.ion_score, pep_id.ppm_error, file, pep_id.match_file, PeakIDMapper.determine_stats(rt_array), PeakIDMapper.determine_stats(mz_array), PeakIDMapper.determine_stats(int_array), rt_array, mz_array, int_array)
+      end
       output_map
     end
   end
   module CommandLine
     def self.run(pepxml, mzml, opts = {})
-      pepdata = PepxmlParser.parse(pepxml)
-      peakIDs = MzmlParser.join_pepxml_with_mzml_file(pepdata, mzml)
+      pep_ids = PepxmlParser.parse(pepxml)
+      peakIDs = MzmlParser.join_pepxml_with_mzml_file(pep_ids, mzml)
     end
   end
 end
